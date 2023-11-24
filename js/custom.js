7fee9efe9718b5a9d3a07d5e529850334bf36454
aurelien <achatelain62000@gmail.com>
2023-11-24 18:05:41 +0100

    gestion automatique des cards

--- js/custom.js
+++ js/custom.js
@@ -7,20 +7,38 @@ const debutDivCardBody = '<div class="card-body">';
 const finDiv = '</div>';
 
 var listeItemPlats = [];
+var listeItemVolaille = [];
+var listeItemBoeuf = [];
+var listeItemVeau = [];
+var listeItemAgneau = [];
+var listeItemPorc = [];
 
-const listePlats = [{"image":"/img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."},
+const listePlats = [
   {"image":"./img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."},
   {"image":"./img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."},
   {"image":"./img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."},
-  {"image":"./img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."}];
+  {"image":"./img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."},
+  {"image":"./img/platsCuisine/choucroute.jpg", "title":"Choucroute", "text":"Une recette traditionnelle et alsacienne, un incontournable pour vos repas en hiver."}
+];
 
-for(let i = 0; i < listePlats.length; i++) {
-  let plat = listePlats[i];
-  var itemPlat = debutDivCol + debutDivCard + '<img src="' + plat.image + '" className="card-img-top" alt="' + plat.title + '">';
-  itemPlat = itemPlat + debutDivCardBody + '<h5 class="card-title">' + plat.title+ '</h5>' + '<p className="card-text">' + plat.text +'</p>';
-  itemPlat = itemPlat + finDiv + finDiv + finDiv;
+const listeVolaille = [
+  {"image":"./img/viandesVolailles/escalope_dinde.jpg", "title":"Escalope de filet de dinde", "text":"Au four ou à la poêle, cuisinez le filet de dinde à la milanaise, au curry, à la moutarde..."},
+  {"image":"./img/viandesVolailles/escalope_dinde.jpg", "title":"Escalope de filet de dinde", "text":"Au four ou à la poêle, cuisinez le filet de dinde à la milanaise, au curry, à la moutarde..."},
+  {"image":"./img/viandesVolailles/escalope_dinde.jpg", "title":"Escalope de filet de dinde", "text":"Au four ou à la poêle, cuisinez le filet de dinde à la milanaise, au curry, à la moutarde..."},
+  {"image":"./img/viandesVolailles/escalope_dinde.jpg", "title":"Escalope de filet de dinde", "text":"Au four ou à la poêle, cuisinez le filet de dinde à la milanaise, au curry, à la moutarde..."}
+];
 
-  listeItemPlats.push(itemPlat);
+remplirDiv(listePlats, listeItemPlats, "plats");
+remplirDiv(listeVolaille, listeItemVolaille, "volaille");
+
+function remplirDiv(liste, listeItem, idDiv){
+  for(let i = 0; i < liste.length; i++) {
+    let item = liste[i];
+    var itemFinal = debutDivCol + debutDivCard + '<img src="' + item.image + '" className="card-img-top" alt="' + item.title + '">';
+    itemFinal = itemFinal + debutDivCardBody + '<h5 class="card-title">' + item.title+ '</h5>' + '<p className="card-text">' + item.text +'</p>';
+    itemFinal = itemFinal + finDiv + finDiv + finDiv;
+
+    listeItem.push(itemFinal);
+  }
+  document.getElementById(idDiv).innerHTML = listeItem.join('\n');
 }
-
-document.getElementById("plats").innerHTML = listeItemPlats.join('\n');
